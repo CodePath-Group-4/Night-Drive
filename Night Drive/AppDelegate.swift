e51ee8f592d9e14e0fcc60031ee0e071dba51acb
Rashon <rashonpoole2323@gmail.com>
2020-11-29 19:24:13 -0500

    connected to back4app

--- Night Drive/AppDelegate.swift
+++ Night Drive/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "8SGBYUJved9knPVNDQe4maHaP2H3HJuKq85f1qkE" // <- UPDATE
+                $0.clientKey = "3fPRtWGDug9L5mfLgyvJLqvfiKLLpcnrMaaLdSW5" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
+        // --- end copy
+
+        
+        
         return true
     }
 
