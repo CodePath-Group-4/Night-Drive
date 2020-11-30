a6fa52845641073a5034bce0ae705efc3209f7f0
gabejsanborn <gabejsanborn@gmail.com>
2020-11-29 19:16:28 -0500

    sign up / login

--- Night Drive/LoginViewController.swift
+++ Night Drive/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
@@ -20,9 +21,22 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password)
+        { (user, error) in
+            if user != nil{
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+            else{
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
     }
     
     @IBAction func onSignUp(_ sender: Any) {
+        self.performSegue(withIdentifier: "signUpSegue", sender: nil)
     }
     
     /*
--- Night Drive/SignUpViewController.swift
+++ Night Drive/SignUpViewController.swift
@@ -6,16 +6,41 @@
 //
 
 import UIKit
+import Parse
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var sUsernameField: UITextField!
+    @IBOutlet weak var sPasswordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func SignUp(_ sender: Any) {
+        
+        let user = PFUser()
+        
+        user.email = emailField.text!
+        user.username = sUsernameField.text!
+        user.password = sPasswordField.text!
+        
+        user.signUpInBackground { (success, error) in
+            if success{
+                self.performSegue(withIdentifier: "signUpCompleteSegue", sender: nil)
+            }
+            else{
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    }
+    
+    @IBAction func SignIn(_ sender: Any) {
+        self.performSegue(withIdentifier: "signUpCompleteSegue", sender: nil)
+    }
     /*
     // MARK: - Navigation
 
