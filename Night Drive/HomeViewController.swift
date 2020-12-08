01585c0b97c27cb489a7980ccf7ea5d01ac51884
Mitchell Schuldinger <mitchellasdf@gmail.com>
2020-12-08 15:52:50 -0500

    username shows on home screen

--- Night Drive/HomeViewController.swift
+++ Night Drive/HomeViewController.swift
@@ -6,27 +6,29 @@
 //
 
 import UIKit
+import Parse
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var userLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: animated)
+        //puts username is "User" field
+        let userInfo =  PFObject(className: "User")
+        userInfo["username"] = PFUser.current()!
+        let user = userInfo["username"] as! PFUser
+        userLabel.text = user.username
+        
+        
+        
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+          
 }
