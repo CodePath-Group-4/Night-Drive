3a5d918cba426cb1347105a51c7254696d15a756
gabejsanborn <gabejsanborn@gmail.com>
2020-12-12 12:36:04 -0500

    music playing with downloaded music

--- Night Drive/MusicViewController.swift
+++ Night Drive/MusicViewController.swift
@@ -6,9 +6,13 @@
 //
 
 import UIKit
-
+import AVFoundation
 class MusicViewController: UIViewController {
 
+    @IBOutlet weak var albumCoverImageView: UIImageView!
+    
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,6 +23,34 @@ class MusicViewController: UIViewController {
         navigationController?.setNavigationBarHidden(true, animated: animated)
     }
 
+    @IBAction func playButton(_ sender: Any) {
+        if let player = player, player.isPlaying{
+            player.stop()
+        }
+        else{
+            
+            let urlString = Bundle.main.path(forResource: "Childish Gambino - Sober", ofType: "mp3")
+            do{
+                try AVAudioSession.sharedInstance().setMode(.default)
+                try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+                
+                guard let urlString = urlString else {
+                    return
+                }
+                
+                player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: urlString))
+                
+                guard let player = player else{
+                    return
+                }
+                
+                player.play()
+            }
+            catch{
+                print("a problem occured")
+            }
+        }
+    }
     /*
     // MARK: - Navigation
 
